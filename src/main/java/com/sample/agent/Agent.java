ffc62130a463e3343b29f161af861c86acc216ed
Dryan Lam <dunglam@kms-technology.com>
2020-09-16 17:35:03 +0700

    Include & Exclude packages

--- src/main/java/com/sample/agent/Agent.java
+++ src/main/java/com/sample/agent/Agent.java
@@ -10,12 +10,15 @@ import net.bytebuddy.implementation.MethodCall;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.SuperMethodCall;
 import net.bytebuddy.matcher.ElementMatchers;
+
 import java.lang.instrument.Instrumentation;
+
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 
 public class Agent {
-        private static final String DEMO_INSTRUMENTED_CLASS_NAME = "com.sample.dl.controller.BookController";
+    private static final String INCLUDE_PACKAGE_INSTRUMENT = "com.sample.dl";
+    private static final String EXCLUDE_PACKAGE_INSTRUMENT = "com.sample.dl.context";
 
     public static void premain(String arguments, Instrumentation instrumentation) {
 
@@ -25,16 +28,16 @@ public class Agent {
         new AgentBuilder.Default()
                 .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
 //                .type((ElementMatchers.any()))
-                .type(named(DEMO_INSTRUMENTED_CLASS_NAME))
+                .type((ElementMatchers.nameStartsWith(INCLUDE_PACKAGE_INSTRUMENT))
+                              .and(ElementMatchers.not(ElementMatchers.nameStartsWith(EXCLUDE_PACKAGE_INSTRUMENT))))
                 .transform((builder, typeDescription, classLoader, module) -> builder
-//                .transform((builder, typeDescription, classLoader, module) -> builder.visit(Advice.to(MethodAdvice.class).on(isMethod()))
-                        .method(ElementMatchers.any())
-                        .intercept(Advice.to(MethodAdvice.class))
-//                        .defineMethod("getAllMethods", void.class, Visibility.PUBLIC)
-//                        .intercept(MethodDelegation.to(MethodAddition.class))
-//                        .method(ElementMatchers.nameContains("method2"))
-//                        .intercept(SuperMethodCall.INSTANCE
-//                                           .andThen(MethodCall.invoke(ElementMatchers.nameContains("getAllMethods"))))
+                                   .method(ElementMatchers.any())
+                                   .intercept(Advice.to(MethodAdvice.class))
+                        .defineMethod("methodAdding", void.class, Visibility.PUBLIC)
+                        .intercept(MethodDelegation.to(MethodAddition.class))
+                        .method(ElementMatchers.nameContains("method2"))
+                        .intercept(SuperMethodCall.INSTANCE
+                                           .andThen(MethodCall.invoke(ElementMatchers.nameContains("methodAdding"))))
                 ).installOn(instrumentation);
     }
 
