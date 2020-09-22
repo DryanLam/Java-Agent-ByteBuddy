175718456bcfde0482124e5ac1d15566bf018c1a
Dryan Lam <dunglam@kms-technology.com>
2020-09-22 16:25:55 +0700

    Cool completed

--- src/main/java/com/sample/dl/context/AppConfiguration.java
+++ src/main/java/com/sample/dl/context/AppConfiguration.java
@@ -1,34 +1,17 @@
 package com.sample.dl.context;
 
 
+import com.sample.dl.controller.BaseController;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.reflections.Reflections;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import javax.ws.rs.Path;
 
 
 @Configuration
 public class AppConfiguration extends ResourceConfig {
     //	public AppConfiguration(@HeaderParam("content-type") String contentType) {
     public AppConfiguration() {
-        scan("com.sample.dl.controller");
+        register(BaseController.class);
     }
 
-    // Also lay on both methods
-    @PostConstruct
-    public void setUp() {
-    }
 
-    public void scan(String... packages) {
-        for (String pack : packages) {
-            Reflections reflections = new Reflections(pack);
-            reflections.getTypesAnnotatedWith(Path.class, true)
-                       .parallelStream()
-                       .forEach((clazz) -> {
-                           register(clazz);
-                       });
-        }
-    }
 }
--- src/main/java/com/sample/dl/controller/BaseController.java
+++ src/main/java/com/sample/dl/controller/BaseController.java
@@ -1,8 +1,6 @@
 package com.sample.dl.controller;
 
-import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import org.springframework.stereotype.Component;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
@@ -15,21 +13,33 @@ public class BaseController {
 
     @GET
     @Produces("application/json")
-    @Path("/ok")
-    public Response appOk(@HeaderParam("testcase") String testCase) {
-        String greet = "App is OK";
+    @Path("/info")
+    public Response appInfo(@HeaderParam("testcase") String testCase) {
+        String greet = "Warning! Your app is HACKED!!!";
         System.out.println(greet);
-        String output = "{'greet': '" + greet + "'}";
+        String output = "{'greet': '" + greet + "', 'status': 'fresh'}";
         return Response.status(200).entity(output).build();
     }
 
+
     @GET
     @Produces("application/json")
-    @Path("/info")
-    public Response appInfo(@HeaderParam("testcase") String testCase) {
-        String greet = "Welcomes to FlashHatch";
+    @Path("/register")
+    public Response appOk(@HeaderParam("testcase") String testCase) {
+        String greet = "App is OK";
         System.out.println(greet);
-        String output = "{'greet': '" + greet + "'}";
+        String output = "{'greet': '" + greet + "', 'status': 'registered'}";
+        return Response.status(200).entity(output).build();
+    }
+
+
+    @GET
+    @Produces("application/json")
+    @Path("/logout")
+    public Response logOut(@HeaderParam("testcase") String testCase) {
+        String greet = "Good bye!!!";
+        System.out.println(greet);
+        String output = "{'greet': '" + greet + "', 'status': 'exit'}";
         return Response.status(200).entity(output).build();
     }
 }
