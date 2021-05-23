c2687e932546ee72be3d9b821248443852e17d3f
dlam <dryanlam@gmai.com>
2021-05-23 17:43:21 +0700

    Well RC

--- src/main/java/com/sample/agent/utils/MongoDb.java
+++ src/main/java/com/sample/agent/utils/MongoDb.java
@@ -4,16 +4,24 @@ import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
 import java.util.List;
+import java.util.Properties;
 
 public class MongoDb {
 
-    final String CONNECTION = "54.254.209.206";
+    private String CONNECTION = "localhost";
     final int PORT = 27017;
 
     private MongoClient dbClient;
     private DB db;
 
     public MongoDb() {
+        Properties prop = System.getProperties();
+        String host = prop.getProperty("mongodb.host");
+        if(host == null){
+            host = "localhost";
+        }
+        CONNECTION = host;
+        System.out.println(CONNECTION);
         dbClient = new MongoClient(CONNECTION, PORT);
     }
 
